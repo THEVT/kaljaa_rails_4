d4b6c6a22a3032f0d56a55cb5403f742bf97d2a5
ryan <rflynn32@gmail.com>
2013-03-02 23:23:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaljaaFb::Application.config.secret_token = 'eeffaf8eb7e9b1ac47c1460ba7119865b0de52c80163def1209fb0ee8557482b1ba8d181164cb0241c2e51f81419a9234f11bd12cfb4b4de481e259d615aedf4'
